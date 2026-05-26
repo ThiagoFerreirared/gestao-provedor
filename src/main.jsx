5e6635a8b2438e9a0b62ac58c25ef30365e1bfc0
Thiago Ferreira <t.redbala@gmail.com>
2026-05-25 21:24:12 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Root } from './App' // Changed this line
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root /> {/* Changed this line */}
+    <App />
   </React.StrictMode>,
 )
